267897d25e7d92c534d3c59101c254106493b77f
RoghS <roghs@cloudstaff.com>
2020-12-19 10:04:48 +0800

    add APi Validation

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
-        public string Email { get; set; }
+
+        [EmailAddress, Required] public string Email { get; set; }
+
+        [Required, RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+             ErrorMessage =
+                 "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
         public string Password { get; set; }
 
     }
